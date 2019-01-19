dae5291f57d7638efd4bdf476c004dc4648024da
Philipp Melab <philipp.melab@gmail.com>
2019-01-19 16:59:19 +0100

    Moved entity converter patch to override in revision_tree module.

--- src/RevisionTreeServiceProvider.php
+++ src/RevisionTreeServiceProvider.php
@@ -5,6 +5,7 @@ namespace Drupal\revision_tree;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\revision_tree\Entity\EntityRepository;
+use Drupal\revision_tree\ParamConverter\RevisionTreeEntityConverter;
 use Symfony\Component\DependencyInjection\Reference;
 
 class RevisionTreeServiceProvider extends ServiceProviderBase {
@@ -14,6 +15,12 @@ class RevisionTreeServiceProvider extends ServiceProviderBase {
       $definition = $container->getDefinition('entity.repository');
       $definition->setClass(EntityRepository::class);
       $definition->addArgument(new Reference('context.repository'));
+    }
+
+    if ($container->hasDefinition('paramconverter.entity')) {
+      $definition = $container->getDefinition('paramconverter.entity');
+      $definition->setClass(RevisionTreeEntityConverter::class);
+      $definition->addArgument(new Reference('entity.repository'));
     }
   }
 
